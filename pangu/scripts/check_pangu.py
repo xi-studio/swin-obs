bbbea763564a7cebe497422084435ed90b1f93bd
JY <jy@JYdeMac-mini.local>
2023-12-11 15:21:55 +0800

    fix

--- pangu/scripts/check_pangu.py
+++ pangu/scripts/check_pangu.py
@@ -9,7 +9,7 @@ file_list = []
 for x in res[24:-24]:
     print(x)
     name = x.split("_")[-1]
-    tname = x.split(".")[0]
+    tname = name.split(".")[0]
     print(tname)
    
     now   = time.mktime(time.strptime(tname, "%Y-%m-%dT%H"))
@@ -20,8 +20,6 @@ for x in res[24:-24]:
     filename_1  = '../data/1_output/surface_1_%s.npy' % tname_1
     filename_6  = '../data/6_output/surface_6_%s.npy' % tname_6
     filename_24 = '../data/24_output/surface_24_%s.npy' % tname_24
-
-
 
     file_list.append((filename_1, filename_6, filename_24))
 
