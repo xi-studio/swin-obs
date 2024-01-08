960b9e6b5fc450ba4ad23e29c534741a8fdbee20
xi-studio <20littletree@gmail.com>
2024-01-08 16:31:05 +0800

    better unet

--- pangu/satelite/sat_dataset.py
+++ pangu/satelite/sat_dataset.py
@@ -15,13 +15,6 @@ class Radars(Dataset):
 
         self.list = filenames 
         self.fake = fake
-        statis = np.load('data/statis.npz')
-        m = np.ones((241*281, 69)) * statis['mean']
-        s = np.ones((241*281, 69)) * statis['std']
-        self.mean = (m.T).reshape((69, 241, 281))
-        self.std  = (s.T).reshape((69, 241, 281))
-
-
 
     def load_sat(self, filename):
         sate = np.load(filename)
@@ -30,42 +23,29 @@ class Radars(Dataset):
 
         return sate
 
-    def load_obs(self, filename):
-        res = np.load(filename)
-        sur = res['surface']
-        upp = res['upper']
- 
-        N, C, W, H = upp.shape
-        upp = upp.reshape((N*C, W, H))
-
-        obs = np.concatenate((sur, upp), axis=0)
-        obs = (obs - self.mean) / self.std
- 
-        return obs
- 
-
     def __getitem__(self, index):
 
         if self.fake!=True:
-            satename = './data/train/sat_2020/' + self.list[index][0]
-            obsname  = './data/train/era5_obs_2020/' + self.list[index][1]
-            sate = self.load_sat(satename).astype(np.float32)
-            obs  = self.load_obs(obsname).astype(np.float32)
+            t0_name = './data/train/sat_2020/' + self.list[index][0]
+            t1_name  = './data/train/sat_2020/' + self.list[index][1]
+            t0 = self.load_sat(t0_name).astype(np.float32)
+            t1 = self.load_obs(t1_name).astype(np.float32)
 
-            sate = resize(sate, (10, 256, 256))
-            obs  = resize(obs, (69, 256, 256))
+            t0 = resize(t0, (10, 256, 256))
+            t1 = resize(t1, (10, 256, 256))
         else:
-            sate = np.ones((10, 256, 256), dtype=np.float32)
-            obs  = np.ones((69, 256, 256), dtype=np.float32)
+            t0 = np.ones((10, 256, 256), dtype=np.float32)
+            t1 = np.ones((10, 256, 256), dtype=np.float32)
 
-        return obs, sate
+        return t0, t1
 
     def __len__(self):
         return len(self.list)
 
 if __name__ == '__main__':
     filename = np.load('data/meta/era5_to_sat_train.npy')
-    a = Radars(filenames=filename, fake=False)
+    #a = Radars(filenames=filename, fake=False)
+    a = Radars(filenames=filename, fake=True)
 
     train_loader = DataLoader(a, batch_size=1, shuffle=True, num_workers=4)
     for x in train_loader:
