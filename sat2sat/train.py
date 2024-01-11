7b7e89b3d67e62f198849d1f7bf9065c26f26486
xi-studio <20littletree@gmail.com>
2024-01-11 11:02:32 +0800

    sat2sat

--- sat2sat/train.py
+++ sat2sat/train.py
@@ -84,10 +84,10 @@ def training_function(args, config):
         accelerator.print(f"epoch {epoch}: {eval_metric:.5f}")
 
         if epoch > 250:
-            accelerator.save_state(f"./logs_sat/checkpoint_{log_time}/epoch_{epoch}")
+            accelerator.save_model(model, f"./logs_sat/checkpoint_{log_time}/epoch_{epoch}", safe_serialization=False)
         if eval_metric < best_acc:
             best_acc = eval_metric
-            accelerator.save_state(f"./logs_sat/checkpoint_{log_time}/best")
+            accelerator.save_model(model, f"./logs_sat/checkpoint_{log_time}/best", safe_serialization=False)
 
 
 def main(): 
