1592d679424ed110c49562dce3585139ea9f285f
JY <jy@JYdeMac-mini.local>
2024-01-16 14:54:08 +0800

    dataset

--- pangu/tonghua/tpu_dataset.py
+++ pangu/tonghua/tpu_dataset.py
@@ -29,11 +29,7 @@ class Radars(Dataset):
 
         return sate
 
-    def load_obs(self, filename):
-        res = np.load(filename)
-        sur = res['surface']
-        upp = res['upper']
- 
+    def load_obs(self, sur, upp):
         N, C, W, H = upp.shape
         upp = upp.reshape((N*C, W, H))
 
@@ -41,30 +37,44 @@ class Radars(Dataset):
         obs = (obs - self.mean) / self.std
  
         return obs
+
+    def load_era5(self, filename):
+        era5 = np.load(filenmae)
  
+        pre = self.load_obs(era5['pre_sur'], era5['pre_uppper'])
+        obs = self.load_obs(era5['obs_sur'], era5['obs_upper'])
+
+        return pre, obs
 
     def __getitem__(self, index):
 
         if self.fake!=True:
-            satename = self.list[index][0]
-            obsname  = './data/train/era5_obs_2020/' + self.list[index][1]
+            era5name = self.list[index][0]
+            satename = self.list[index][1]
+
             sate = self.load_sat(satename).astype(np.float32)
-            obs  = self.load_obs(obsname).astype(np.float32)
+            pre, obs = self.load_era5(era5name)
+            pre = pre.astype(np.float32)
+            obs = obs.astype(np.float32)
 
             sate = resize(sate, (10, 256, 256))
+            pre  = resize(pre, (69, 256, 256))
+
+            input_data = np.concatenate((sate, pre), axis=0)
             obs  = resize(obs, (69, 256, 256))
         else:
-            sate = np.ones((69, 256, 256), dtype=np.float32)
-            obs  = np.ones((79, 256, 256), dtype=np.float32)
+            input_data = np.ones((79, 256, 256), dtype=np.float32)
+            obs  = np.ones((69, 256, 256), dtype=np.float32)
 
-        return obs, sate
+        return input_data, obs
 
     def __len__(self):
         return len(self.list)
 
 if __name__ == '__main__':
-    filename = np.load('data/meta/era5_to_sat_train.npy')
-    a = Radars(filenames=filename, fake=False)
+    #filename = np.load('data/pan72_meta.npy')
+    a = Radars(filenames=np.arange(100), fake=True)
+    #a = Radars(filenames=filename, fake=False)
 
     train_loader = DataLoader(a, batch_size=1, shuffle=True, num_workers=4)
     for x in train_loader:
