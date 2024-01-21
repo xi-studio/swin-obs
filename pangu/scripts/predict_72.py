64996057b12656f0849a5d9c4a08f8dd60fcd295
xi-studio <20littletree@gmail.com>
2024-01-21 00:32:11 +0000

    pangu 202021 fp16

--- pangu/scripts/predict_72.py
+++ pangu/scripts/predict_72.py
@@ -27,8 +27,8 @@ def main():
     variables = OrderedDict(load_vars('./pangu_config.yaml'))
     surface_data = ar_full_37_1h[variables['input']['surface']]
     high_data = ar_full_37_1h[variables['input']['high']].sel(level=variables['input']['levels'])
-    sur  = surface_data.sel(time=slice('2020-01-01', '2021-12-31'))
-    hig  = high_data.sel(time=slice('2020-01-01', '2021-12-31'))
+    sur  = surface_data.sel(time=slice('2020-01-01', '2021-12-31'), latitude=slice(60.0, 0.0), longitude=slice(70.0, 140.0))
+    hig  = high_data.sel(time=slice('2020-01-01', '2021-12-31'), latitude=slice(60.0, 0.0), longitude=slice(70.0, 140.0))
     sur_dat = xr.concat([sur['mean_sea_level_pressure'],sur['10m_u_component_of_wind'],sur['10m_v_component_of_wind'], sur['2m_temperature']],"var")
     hig_dat = xr.concat([hig['geopotential'],hig['specific_humidity'],hig['temperature'],hig['u_component_of_wind'],hig['v_component_of_wind']],'var')
 
@@ -44,13 +44,10 @@ def main():
         high = res_high[i].to_numpy()
 
         sur = sur.astype(np.float16)
-        high = sur.astype(np.float16)
+        high = high.astype(np.float16)
 
-        obs_sur = sur[:, 30*4:90*4+1, 70*4:140*4+1]
-        obs_upper = high[:, :, 30*4:90*4+1, 70*4:140*4+1]
-        np.savez('./pangu_2020_2021/%s.npy' % now, sur=obs_sur, upper=high)
+        np.savez('./pangu_2020_2021/%s.npz' % now, sur=sur, upper=high)
         print('save', now)
-        break
 
 
 
